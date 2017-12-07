3142e8a702bd66f893264e09a6195be965bec840
Dan Phillips <danmakenoise@gmail.com>
2017-12-07 17:01:56 -0500

    add test converage for public api of articleMetadataFromOpts()

--- test/article-metadata-from-opts.test.js
+++ test/article-metadata-from-opts.test.js
@@ -8,5 +8,54 @@ test('articleMetadataFromOpts()', function (t) {
     'it is a function'
   );
 
+  t.is(
+    articleMetadataFromOpts({ isPreview: true }).isPreview,
+    true,
+    'it returns isPreview=true when opts.isPreview is true'
+  );
+
+  t.is(
+    articleMetadataFromOpts({ isPreview: false }).isPreview,
+    false,
+    'it returns isPreview=false when opts.isPreview is false'
+  );
+
+  t.is(
+    articleMetadataFromOpts({}).isPreview,
+    true,
+    'it returns isPreview=true when opts.isPreview is missing'
+  );
+
+  t.is(
+    articleMetadataFromOpts({ isSponsored: true }).isSponsored,
+    true,
+    'it returns isSponsored=true when opts.isSponsored is true'
+  );
+
+  t.is(
+    articleMetadataFromOpts({ isSponsored: false }).isSponsored,
+    false,
+    'it returns isSponsored=false when opts.isSponsored is false'
+  );
+
+  t.is(
+    articleMetadataFromOpts({}).isSponsored,
+    false,
+    'it returns isSponsored=false when opts.isSponsored is missing'
+  );
+
+  t.is(
+    articleMetadataFromOpts({}).sections,
+    undefined,
+    'it does not return a sections property when sections have not been supplied'
+  );
+
+  const sections = ['foo', 'bar', 'baz'];
+  t.deepEqual(
+    articleMetadataFromOpts({ sections: sections }).links.sections,
+    sections,
+    'it sets links.sections to the supplied sections array'
+  );
+
   t.end();
 });
